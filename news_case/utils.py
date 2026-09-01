a80b7c67621481d213a20a3f01a1d687662ada7c
kikyo <ldh001202@gmail.com>
2026-09-01 09:52:46 +0800

    同步 Gitee 更新: 正则清洗函数完整版(shuiruohanyu/python_big_case 0aa9eff)

--- news_case/utils.py
+++ news_case/utils.py
@@ -1,4 +1,5 @@
 # 闭包计数器
+import re
 import time
 from functools import wraps
 
@@ -31,4 +32,28 @@ def timer(label):
 
     return outer
 
+
 # 正则清洗函数
+# 从网络上爬取的内容 需要进行清洗
+# <![CDATA[示例：我国多地发布稳就业新举措]]> -> 示例：我国多地发布稳就业新举措
+# () () () \1 \2 \3
+def clean_text(text):
+    # 这只是第一种情况
+    # <![CDATA[示例：我国多地发布稳就业新举措]]> -> 示例：我国多地发布稳就业新举措
+
+    text = re.sub(r"<!\[CDATA\[(.*?)\]\]>", r"\1", text)
+    # <a>你好我是老高</a> -> 你好我是老高
+    text = re.sub(r"<.*?>", "", text)
+    # 去除一些空格
+    # 我是你            爸爸         的       学生 -> 我是你 爸爸 的 学生
+    text = re.sub(r"\s+", " ", text)
+
+    # 百度 新浪 网易
+    return text.strip()
+
+
+print(clean_text("<![CDATA[示例：我国多地发布稳就业新举措]]>"))
+
+print(clean_text("<a>我是你爸爸</a>"))
+
+print(clean_text("我是你            爸爸         的       学生"))
